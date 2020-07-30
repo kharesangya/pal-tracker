99a8201df05734c90615850451298a309355ca6b
Sangya <sangya.khare@gmail.com>
2020-07-30 15:36:23 +0000

    created db scriptJDBC Spring Template lab

---
+++ src/main/java/io/pivotal/pal/tracker/MysqlDataSource.java
@@ -0,0 +1,10 @@
+package io.pivotal.pal.tracker;
+
+public class MysqlDataSource {
+    private String datasourceUrl;
+
+    public void setUrl(String datasourceUrl) {
+
+        this.datasourceUrl = datasourceUrl;
+    }
+}
